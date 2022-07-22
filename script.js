9c7681bae6b3014c34718606c2aa30a3a2fe865f
muskan2532150 <rg2532150@gmail.com>
2022-07-22 16:26:24 +0530

    Potfolio demo

---
+++ script.js
@@ -0,0 +1,60 @@
+const navright = document.querySelector('.navright');
+const burger = document.querySelector('.burger');
+const closeBtn = document.querySelector('.close-btn');
+const navLink = document.querySelectorAll('.nav-link');
+
+burger.onclick = function () {
+  navright.classList.toggle('click');
+  document.body.style.overflow = 'hidden';
+};
+
+closeBtn.onclick = function () {
+  navright.classList.remove('click');
+  document.body.style.overflow = 'visible';
+};
+
+navLink.forEach((link) => {
+  link.onclick = function () {
+    navright.classList.remove('click');
+    document.body.style.overflow = 'visible';
+  };
+});
+
+//   let email = document.forms.email.value;
+  
+// let  formObject = {
+//   name : String,
+//   emails : String,
+//   Comment : String,
+// }
+
+// function formDetail(event) {
+//   event.preventDefault();
+//   let fname = document.getElementById("name").value;
+// let femail = document.getElementById("email").value;
+// let fComment = document.getElementById("text-area").value;
+
+// formObject.name=fname;
+// formObject.emails=femail;
+// formObject.Comment=fComment;
+// // localStorage.clear();
+// localStorage.setItem("form Details", JSON.stringify(formObject));
+// }
+
+// let formF =document.getElementById('formSubmit');
+
+// formF.addEventListener('submit',(ev) => {
+//   let z = document.getElementById("error-message");
+//    if (femail !== femail.toLowerCase()){  
+//      ev.preventDefault();
+//     z.innerText = "dxde";
+//     } 
+// formDetail(ev);
+// })
+
+// let inputs = document.querySelectorAll('input');
+// inputs.forEach((inputS) => {
+//   inputS.addEventListener('change',(ev)=>formDetail(ev));
+// })
+
+// fComment.addEventListener('change',(ev)=>formDetail(ev));
